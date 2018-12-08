580b4684589b490cee8397334857e8258b575fc8
Daniel McGiff <daniel.mcgiff@digital-su.co.uk>
2018-12-08 23:54:45 +0000

    syntax fix

--- application/models/AlexaModel.php
+++ application/models/AlexaModel.php
@@ -23,7 +23,7 @@ class AlexaModel extends CI_Model
 		$this->load->model('LoggerModel');
 		
 		$this->LoggerModel->alexaRequestEntry(print_r($inputArr->request,TRUE),'DEBUG');
-		$this->LoggerModel->alexaRequestEntry('Application ID: ' . $applicationID,'DEBUG')
+		$this->LoggerModel->alexaRequestEntry('Application ID: ' . $applicationID,'DEBUG');
 
 		switch($applicationID)
 		{
