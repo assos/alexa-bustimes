198c3a9709fa52496a68adccb1f2edf517607634
Daniel McGiff <daniel.mcgiff@digital-su.co.uk>
2018-12-09 00:59:14 +0000

    fleshed out security checks

--- application/models/AlexaModel.php
+++ application/models/AlexaModel.php
@@ -8,25 +8,124 @@
  *	ORIGINAL AUTHOR:	Daniel McGiff											*
  *	CONTRIBUTORS:																*
  *	DATE CREATED:		7th December 2018										*
- *	DATE MODIFIED:		7th December 2018										*
+ *	DATE MODIFIED:		9th December 2018										*
  *																				*
  ********************************************************************************/
  
 class AlexaModel extends CI_Model
 {
-	public function securityCheck()
+
+	function valid_cert($jsonRequest) 
 	{
-		$inputArr = json_decode($this->input->raw_input_stream);
+		/********************************************************************************	
+		 *																				*
+		 *	NAME: 			valid_cert													*
+		 *	TYPE:			Function													*
+		 *	DESCRIPTION: 	Validates Alexa SSL Cert from Request.						*
+		 *																				*
+		 *	ORIGINAL AUTHOR:	rbowen (https://github.com/rbowen)						*
+		 *	TAKEN FROM:			https://github.com/rbowen/validate-echo-request-php		*		
+		 *																				*
+		 *	INPUTS:				jsonRequest (string) (required)							*
+		 *																				*
+		 ********************************************************************************/
+
+		$data = json_decode($jsonRequest,true);
+		$ECHO_CERT_CACHE = FCPATH . 'application/cache/';
+				 																
+	    // Determine if we need to download a new Signature Certificate Chain from Amazon
+    	$md5pem = $ECHO_CERT_CACHE . md5($_SERVER['HTTP_SIGNATURECERTCHAINURL']) . '.pem';
+		$echoServiceDomain = 'echo-api.amazon.com';
+
+	    // If we haven't received a certificate with this URL before,
+    	// store it as a cached copy
+	    if (!file_exists($md5pem))
+	    {
+        	file_put_contents($md5pem, file_get_contents($_SERVER['HTTP_SIGNATURECERTCHAINURL']));
+    	}
+
+	    // Validate certificate chain and signature
+    	$pem = file_get_contents($md5pem);
+		$ssl_check = openssl_verify( $jsonRequest, base64_decode($_SERVER['HTTP_SIGNATURE']), $pem, 'sha1' );
+		if ($ssl_check != 1) 
+		{
+			$this->Logger->Model->alexaRequestEntry('Certificate Verification Failed: ' . openssl_error_string(),'NOTICE');
+			return false;
+    	}
+
+		// Parse certificate for validations below
+    	$parsedCertificate = openssl_x509_parse($pem);
+    	if (!$parsedCertificate) 
+    	{
+			$this->Logger->Model->alexaRequestEntry('Certificate Verification Failed: x509 Parsing Failure','NOTICE');
+			return false;
+    	}
+
+		// Check that the domain echo-api.amazon.com is present in
+    	// the Subject Alternative Names (SANs) section of the signing certificate
+    	if(strpos( $parsedCertificate['extensions']['subjectAltName'],$echoServiceDomain) === false) 
+    	{
+			$this->Logger->Model->alexaRequestEntry('Certificate Verification Failed: subjectAltName Check Failed','NOTICE');
+			return false;
+    	}
+
+	    // Check that the signing certificate has not expired
+    	// (examine both the Not Before and Not After dates)
+	    $validFrom = $parsedCertificate['validFrom_time_t'];
+    	$validTo = $parsedCertificate['validTo_time_t'];
+		$time = time();
+		
+		if (!($validFrom <= $time && $time <= $validTo)) 
+		{
+			$this->Logger->Model->alexaRequestEntry('Certificate Verification Failed: Expiration Check Failed','NOTICE');
+			return false;
+    	}
+
+		return true;
+	}
+	
+	public function securityCheck($skillID)
+	{
+		$inputRaw = $this->input->raw_input_stream;
+		$inputArr = json_decode($inputRaw);
 		
 		$applicationID = $inputArr->session->application->applicationId;
-		
+		$requestTimestamp = $inputArr->request->timestamp;
+		$signatureCertChainURL = $_SERVER['HTTP_SIGNATURECERTCHAINURL'];
+
 		$this->load->model('LoggerModel');
 		
 		$this->LoggerModel->alexaRequestEntry(print_r($inputArr->request,TRUE),'DEBUG');
 		$this->LoggerModel->alexaRequestEntry('Application ID: ' . $applicationID,'DEBUG');
+		$this->LoggerModel->alexaRequestEntry('Request Certificate URL: ' . $signatureCertChainURL,'DEBUG');
 
-		switch($applicationID)
+		//First Security Check - Does the ApplicationID passed in the request match the SkillID passed in the function call.
+		if($skillID != $applicationID)
 		{
+			$securityFailed = true;
+			$this->Logger->Model->alexaRequestEntry('Invalid application ID. Passed ' . $applicationID . ' expecting ' . $skillID,'NOTICE');
+		}
+
+		//Second Security Check - Is the timestamp valid?
+		if($requestTimestamp <= date('Y-m-d\TH:i:s\Z', time()-150))
+		{
+			$securityFailed = true;
+			$this->Logger->Model->alexaRequestEntry('Invalid request timestamp' . $requestTimestamp,'NOTICE');
+		}
+		
+		//Third Security Check - Is the Certificate Valid?
+		if(!$this->valid_cert($inputRaw))
+		{
+			$securityFailed = true;
+		}
+		
+		if($securityFailed)
+		{
+		
+			case "amzn1.ask.skill.18bfabef-4265-4b8e-a666-1b588a0077d6":
+			
+			break;
+			
 			default:
 			
 				header ('Content-Type: application/json');
@@ -37,6 +136,11 @@ class AlexaModel extends CI_Model
 				$responseArr['response']['outputSpeech']['type'] = 'PlainText';
 				$responseArr['response']['outputSpeech']['text'] = 'Hmm... Do I know you?';
 				echo json_encode($responseArr, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+		}
+		else
+		{
+			$this->Logger->Model->alexaRequestEntry('Security Checks Passed','INFO');
+			return true;
 		}
 		
 	}
