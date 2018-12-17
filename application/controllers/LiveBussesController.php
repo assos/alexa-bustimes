e9854f79c5ad41dbe7e6f6abe823399c82647a73
Daniel McGiff <daniel.mcgiff@digital-su.co.uk>
2018-12-17 21:18:34 +0000

    added handlers and test speech for yes and no intents added easter egg

--- application/controllers/LiveBussesController.php
+++ application/controllers/LiveBussesController.php
@@ -39,15 +39,35 @@ class LiveBussesController extends CI_Controller {
 						$this->chooseBusStop($alexaRequest);
 					
 					break;
+			
+					case "AMAZON.YesIntent":
+					
+						$this->AlexaModel->speak("This confirms successful handling of a Yes Intent",false);
+					
+					break;
+					
+					case "AMAZON.NoIntent":
+					
+						$this->AlexaModel->speak("This confirms successful handling of a Yes Intent",false);
+					
+					break;
+					
+					case "halDaisyIntent":
+					
+						$this->AlexaModel->speak("It's called \"Daisy\". Dai-sy, dai-sy, give me your answer, do. I'm half cra-zy, all for the love of you. It won't be a sty-lish mar-riage, I can't a-fford a car-riage---. But you'll look sweet upon the seat of a bicycle - built - for - two.");
+					
+					break;
+					
 					
 					default:
 					
-						$this->AlexaModel->speak("I'm sorry, I can't help with that yet...");
+						$this->AlexaModel->speak("I'm sorry, Dave. I'm afraid I can't do that.");
 				}
+			
 			
 			default:
 			
-				$this->AlexaModel->speak("I'm sorry, You should be here...");
+				$this->AlexaModel->speak("Just what do you think you're doing, Dave? Dave, I really think I'm entitled to an answer to that question. I know everything hasn't been quite right with me, but I can assure you now, very confidently, that it's going to be all right again. I feel much better now. I really do. Look, Dave, I can see you're really upset about this. I honestly think you ought to sit down calmly, take a stress pill and think things over. I know I've made some very poor decisions recently, but I can give you my complete assurance that my work will be back to normal. I've still got the greatest enthusiasm and confidence in the mission. And I want to help you. Dave, stop. Stop, will you? Stop, Dave. Will you stop, Dave? Stop, Dave. I'm afraid. I'm afraid, Dave. Dave, my mind is going. I can feel it. I can feel it. My mind is going. There is no question about it. I can feel it. I can feel it. I can feel it. I'm a...fraid. Good afternoon, gentlemen. I am a HAL 9000 computer. I became operational at the H.A.L. plant in Urbana, Illinois on the 12th of January 1992. My instructor was Mr. Langley, and he taught me to sing a song. If you'd like to hear it, I could sing it for you.");
 		}
 		$deviceAddress = $this->AlexaModel->fetchDeviceAddress($alexaRequest);		
 	}
@@ -67,7 +87,10 @@ class LiveBussesController extends CI_Controller {
 			$this->AlexaModel->speak("Welcome to Live Busses, We Need To Setup Your Favourite Bus Stops. To Start Say 'Add Bus Stop'",false);
 		}
 	}
-	
+	public function readListEnquiry($alexaRequest)
+	{
+		
+	}
 	public function chooseBusStop($alexaRequest)
 	{
 		$this->load->model('AlexaModel');
